92c33e5d6a4e3dde9d9f4ad1bdace6fbe9e84143
continnd <continnd@miamioh.edu>
2015-03-22 14:56:45 -0400

    hw6 code properly formatted

--- hw6/continnd_hw6.cpp
+++ hw6/continnd_hw6.cpp
@@ -1,9 +1,12 @@
+// Copyright 2015 Nick Contini
+//
+#include <math.h>
+#include <omp.h>
 #include <string>
 #include <iterator>
 #include <iostream>
 #include <vector>
-#include <math.h>
-#include <omp.h>
+#include <algorithm>
 
 #define PRIME 128
 #define FACTORABLE 64
@@ -11,21 +14,15 @@
 #define PALLINDROME 16
 
 char isPrime(int num) {
-    if (num < 2) {
-        return 0;
-    }
+    if (num < 2) { return 0; }
     for (int i = 2; i <= sqrt(num); i++) {
-        if (num % i == 0) {
-            return 0;
-        }
+        if (num % i == 0) { return 0; }
     }
     return PRIME;
 }
 
 char hasPrimeFactors(int num) {
-    if (num < 4) {
-        return 0;
-    }
+    if (num < 4) { return 0; }
     for (int i = 2; i < sqrt(num); i++) {
         if (num % i == 0 && isPrime(i) && isPrime(num / i)) {
             return FACTORABLE;
@@ -46,9 +43,7 @@ char isPallindrome(int num) {
     std::vector<int>::iterator digitsEnd = digits.begin() + digits.size() / 2;
 
     for (; it != digitsEnd; it++, rit++) {
-        if (*it != *rit) {
-            return 0;
-        }
+        if (*it != *rit) { return 0; }
     }
     return PALLINDROME;
 }
@@ -56,42 +51,13 @@ char isPallindrome(int num) {
 char isFactorial(int num) {
     int i = 2;
     while (num != 1) {
-        if (num % i != 0) {
-            return 0;
-        }
-        num /= i;
-        i++;
+        if (num % i != 0) { return 0; }
+        num /= i++;
     }
     return FACTORIAL;
 }
 
-int main(int argc, char const* argv[])
-{
-    if (argc != 3 || std::atoi(argv[1]) > std::atoi(argv[2])) {
-        std::cout << "Not a valid range!" << std::endl;
-        std::exit(1);
-    }
-
-    int begin = std::atoi(argv[1]);
-    int end = std::atoi(argv[2]);
-
-    std::vector<char> numInfo(end - begin + 1);
-
-#pragma omp parallel
-    {
-        unsigned int numThr = omp_get_num_threads();
-        unsigned int threadID = omp_get_thread_num();
-        unsigned int chunkSize = numInfo.size() / numThr + 1;
-        unsigned int thrStart = threadID * chunkSize;
-        unsigned int thrEnd = (thrStart + chunkSize) < numInfo.size() ? (thrStart
-            + chunkSize) : numInfo.size();
-        for (unsigned int i = thrStart; i < thrEnd; i++) {
-            numInfo[i] += isPrime(i + begin);
-            numInfo[i] += hasPrimeFactors(i + begin);
-            numInfo[i] += isFactorial(i + begin);
-            numInfo[i] += isPallindrome(i + begin);
-        }
-    }
+void printResults(std::vector<char>& numInfo, int begin) {
     for (unsigned int i = 0; i < numInfo.size(); i++) {
         std::printf("%d: %sprime, %sfactorable, %sfactorial, %spallindrome\n",
                 begin + i, (numInfo[i] & PRIME ? "is " : "not "),
@@ -99,5 +65,30 @@ int main(int argc, char const* argv[])
                 (numInfo[i] & FACTORIAL ? "is " : "not "),
                 (numInfo[i] & PALLINDROME ? "is " : "not "));
     }
+}
+
+int main(int argc, char const* argv[]) {
+    if (argc != 3 || std::atoi(argv[1]) > std::atoi(argv[2])) {
+        std::cout << "Not a valid range!" << std::endl;
+        std::exit(1);
+    }
+
+    int begin = std::atoi(argv[1]), end = std::atoi(argv[2]);
+
+    std::vector<char> numInfo(end - begin + 1);
+
+#pragma omp parallel
+    {
+        unsigned int chunkSize = numInfo.size() / omp_get_num_threads() + 1;
+        unsigned int thrStart = omp_get_thread_num() * chunkSize;
+        unsigned int thrEnd = std::min((thrStart + chunkSize), (unsigned int)
+                numInfo.size());
+        for (unsigned int i = thrStart; i < thrEnd; i++) {
+            numInfo[i] += isPrime(i + begin) + hasPrimeFactors(i + begin);
+            numInfo[i] += isFactorial(i + begin) + isPallindrome(i + begin);
+        }
+    }
+
+    printResults(numInfo, begin);
     return 0;
 }
